9130585b4e8b2d60f5be4a2a2c4e51516c8bb111
czdemo <210707712@qq.com>
2025-03-26 10:56:50 +0800

    1111

--- webserver/config/configs.py
+++
@@ -1,6 +0,0 @@
-#数据库信息
-mysqlurl="118.25.137.23"
-mysqlport="3306"
-mysqlname="gptapp"
-mysqlaccount="gptapp"
-mysqlpassword="gptapp"
--- webserver/pdftest.py
+++
@@ -1,5 +0,0 @@
-import pymupdf # imports the pymupdf library
-doc = pymupdf.open("“老十针”为主治疗脓毒症合并急性胃肠损伤的临床研究_廖丹.pdf") # open a document
-for page in doc: # iterate the document pages
-  text = page.get_text() # get plain text encoded as UTF-8
-  print(text)
